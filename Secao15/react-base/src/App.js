c5499c290c478ded22fa60bb748880fe563f97ad
Tony-Starkus <thalisson.bandeira@hotmail.com>
2022-08-18 13:25:23 -0500

    updates Seção 15

--- Secao15/react-base/src/App.js
+++ Secao15/react-base/src/App.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Router } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import history from './services/history';
 import Header from './components/Header';
-
+import store, { persistor } from './store';
 import Routes from './routes';
 
 import GlobalStyles from './styles/GlobalStyles';
 
 function App() {
   return (
-    <Router history={history}>
-      <Header />
-      <Routes />
-      <GlobalStyles />
-      <ToastContainer
-        autoClose={3000}
-        className="toast-container"
-        icon={false}
-      />
-    </Router>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Header />
+          <Routes />
+          <GlobalStyles />
+          <ToastContainer
+            autoClose={3000}
+            className="toast-container"
+            icon={false}
+          />
+        </Router>
+      </PersistGate>
+    </Provider>
   );
 }
 
--- Secao15/react-base/src/pages/Login/index.js
+++ Secao15/react-base/src/pages/Login/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from '../../styles/GlobalStyles';
 import { Title } from './styled';
 
+import * as exampleActions from '../../store/modules/example/actions';
+
 export default function Login() {
+  const dispatch = useDispatch();
+  function handleClick() {
+    dispatch(exampleActions.clicaBotaoRequest());
+  }
+
   return (
     <Container>
       <Title>Login</Title>
       <p>atapo</p>
-      <button type="button">Enviar</button>
+      <button type="button" onClick={handleClick}>
+        Enviar
+      </button>
     </Container>
   );
 }
